f9055d6f367a7ff480d1f79be5ebb9f68a350a52
Dababy <Dababy@DESKTOP-79IU4EK>
2021-11-11 09:23:13 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+    { //3.2  сложный уровень 10 вариант
+        try
+        {
+            Console.WriteLine("Введите n:");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                int s = 0;
+                string res = "";
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        s += j;
+                        res += j + "+";
+                    }
+                }
+                if (i == s) Console.WriteLine(res + "=" + i);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
+}
